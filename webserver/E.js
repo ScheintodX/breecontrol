ed960bf3fe4d7fff61e110b9cf471ae5ca3853db
Linux User <braumeister@braumeister.kabuff.org>
2023-12-07 00:30:05 +0000

    E.xx

--- webserver/E.js
+++ webserver/E.js
@@ -1,16 +1,37 @@
 import 'colors';
 
-function stack( depth ) {
+function stack(numberOfLinesToRemove = 3, maxLines = -1) {
 
 	try {
-		throw Error( 'stack' );
-	} catch( err ) {
-		var line = err.stack.split('\n')[ depth ].trim();
+		throw new Error();
+	} catch (error) {
 
-		line = line.replace( /^at /, '' );
-		line = line.replace( /\/.*\/(.*.js)/g, '$1' );
+		let stackLines = error.stack
+			.split('\n')
+			.slice(numberOfLinesToRemove + 1)
+			.filter(line => !line.includes('node_modules'));
 
-		return line;
+		if (maxLines !== -1) {
+			stackLines = stackLines.slice(0, maxLines);
+		}
+
+		return stackLines.map( line => {
+			let functionName = 'unknown';
+			let fileName = 'unknown';
+
+			// Check for function name and file details
+			const functionMatch = line.match(/at (\S+)(?: \[as \S+\])? \(/); //line.match(/at (.+) \(/);
+			if (functionMatch && functionMatch[1]) {
+				functionName = functionMatch[1].trim();
+			}
+
+			const fileMatch = line.match(/\/([^\/]+:\d+):\d+/); //line.match(/\/([^\/]+:\d+)/);
+			if (fileMatch && fileMatch[1]) {
+				fileName = fileMatch[1].trim();
+			}
+
+			return `${functionName}[${fileName}]`;
+		} ).join(' < ');
 	}
 }
 
@@ -30,6 +51,15 @@ export const E = {
 		var args = Array.prototype.slice.call( arguments ); //clone
 
 		args.unshift( stack( 3 ).green );
+
+		console.log.apply( console, args );
+	},
+
+	// E.x( 5, "fubar" )
+	x: function(){
+		var args = Array.prototype.slice.call( arguments );
+
+		args.unshift( stack( 2, 3 ).red );
 
 		console.log.apply( console, args );
 	}
